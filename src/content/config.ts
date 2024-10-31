842aa221d20e32bc9ad44cd62100834bf2cca047
wcgordon1 <wcgordon1@gmail.com>
2024-10-30 20:39:59 -0700

    json-ld working now time for josb and search.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,84 +1,49 @@
 import { defineCollection, z } from "astro:content";
-const integrations = defineCollection({
-  schema: z.object({
-    integration: z.string(),
-    description: z.string(),
-    email: z.string(),
-    permissions: z.array(z.string()),
-    details: z.array(z.object({
-      title: z.string(),
-      value: z.string(),
-      url: z.optional(z.string()),
-    })),
-    logo: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-  }),
-});
-const helpcenter = defineCollection({
-  schema: z.object({
-    page: z.string(),
-    description: z.string(),
-    icon: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-  }),
-});
-const customers = defineCollection({
-  schema: z.object({
-    customer: z.string(),
-    challengesAndSolutions: z.array(z.object({
-      title: z.string(),
-      content: z.string(),
-    })),
-results: z.array(z.string()),
-    about: z.string(),
-    details: z.record(z.string()),
-    logo: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-  }),
-});
-const infopages = defineCollection({
-  schema: z.object({
-    page: z.string(),
-    pubDate: z.date().optional(),
-  }),
-});
-const jobs = defineCollection({
+
+const jobsCollection = defineCollection({
   schema: z.object({
     position: z.string(),
     location: z.string(),
     team: z.string(),
-    flag: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-
-  }),
-});
-const postsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
+    employmentType: z.enum([
+      "FULL_TIME",
+      "PART_TIME",
+      "CONTRACTOR",
+      "TEMPORARY",
+      "INTERN",
+      "VOLUNTEER",
+      "PER_DIEM",
+      "OTHER"
+    ]),
+    datePosted: z.string(),
+    validThrough: z.string(),
     description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
+    experienceRequirements: z.string().optional(),
+    occupationalCategory: z.string().optional(),
+    salary: z.object({
+      currency: z.string(),
+      value: z.number(),
+      unitText: z.enum(["HOUR", "DAY", "WEEK", "MONTH", "YEAR"])
     }),
-    tags: z.array(z.string()),
-  }),
+    companyDetails: z.object({
+      name: z.string(),
+      url: z.string(),
+      logo: z.string()
+    }),
+    address: z.object({
+      street: z.string(),
+      city: z.string(),
+      region: z.string(),
+      postalCode: z.string(),
+      country: z.string()
+    }),
+    identifier: z.object({
+      name: z.string(),
+      value: z.string()
+    })
+  })
 });
 
 export const collections = {
-  integrations: integrations,
-  helpcenter: helpcenter,
-  customers: customers,
-  infopages: infopages,
-  jobs: jobs,
-  posts: postsCollection,
+  jobs: jobsCollection,
 };
